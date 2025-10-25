cdb85a3bd0ba516e82e4fc6457e4845ff594778f
Cam <herreracamila1304@gmail.com>
2025-10-25 01:22:11 -0300

    Created a React App.

---
+++ app/src/components/Nav.jsx
@@ -0,0 +1,24 @@
+export default function NavBar() {
+
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href=".html">sobre este sitio</a></li>
+                <ul>
+                    <li><a href=".html">contacto</a></li>
+                    <li><a href=".html">tsuki</a></li>
+                </ul>
+                <li><a href=".html">historia</a></li>
+                <ul>
+                    <li><a href=".html">miembros</a></li>
+                </ul>
+                <li><a href=".html">galeria</a></li>
+                <ul>
+                    <li><a href=".html">filtro</a></li>
+                </ul>
+                <li><a href=".html">info</a></li>
+            </ul>
+        </nav>
+    );
+}
