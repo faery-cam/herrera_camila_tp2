136f1692b82cf53e6461bba5f34266f3064cfaf2
Cam <herreracamila1304@gmail.com>
2025-10-26 00:33:22 -0300

    [ADD] agregar react-router para la navegación del sitio

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react';
 import './styles/index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <p>hola</p>
--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,1 +1,10 @@
-import NavBar from './nav.jsx';
+import NavBar from './NavBar.jsx';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>TITULO</h1>
+            <NavBar />
+        </header>
+    )
+}
