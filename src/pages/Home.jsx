8f9b169bc84d1e81e73dcbe652cb01b290809a7b
Cam <herreracamila1304@gmail.com>
2025-11-17 04:12:02 -0300

    [STYLE] estilos para pag home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,19 @@ export default function Home() {
   useEffect(() => {
     document.title = "Información";
   }, []);
-  
+
   return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <div className="fixed inset-0 bg-cover bg-no-repeat -z-10 bg-position-[center_20%] pointer-events-none" style={{ backgroundImage: `url(${getAsset('assets/img/antsa.webp')})` }} />
 
-    <div className="h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${getAsset('assets/img/antsa.webp')})` }}>
-      <div>
-        overlay placeholder
+        <div> 
+          <div className="relative flex flex-col items-center justify-center h-full text-center text-neutral-100 mt-[20vh] pointer-events-auto bg-black/30 p-4 rounded-lg max-w-sm mx-auto">
 
-        <p>¿Es tu primera vez acá? Hacé click <Link to="/info">aquí</Link> para una guía!</p>
+
+            <p className="text-xl mb-2">¿Es tu primera vez acá? </p>
+            <p className="text-lg"> Hacé click <Link to="/info" className="underline underline-offset-4 hover:text-pink-300 transition">aquí</Link>{" "} para una guía!</p>
+          </div>
+        </div>
       </div>
-    </div>
-
   )
-
 }
