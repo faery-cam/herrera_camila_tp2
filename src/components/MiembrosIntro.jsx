6ed64804d3a3b6cc341aad1b7756c1afede5ab25
Cam <herreracamila1304@gmail.com>
2025-11-17 04:40:41 -0300

    [STYLE] pequeña modificación en intro para miembros

--- src/components/MiembrosIntro.jsx
+++ src/components/MiembrosIntro.jsx
@@ -4,22 +4,27 @@ import { getAsset } from "../utils/assets.js";
 export default function MiembrosIntro({ onSelect }) {
 
     return (
-        <div className="flex gap-4 overflow-x-auto py-4 px-2 scrollbar-none">
-            {miembrosData.map((member) => (
-                <div
-                    key={member.id}
-                    className="relative shrink-0 w-65 h-auto rounded-lg overflow-hidden cursor-pointer group hover:scale-105 "
-                    onClick={() => onSelect(member.id)}>
-                    <img
+        <div className="flex flex-col gap-4">
+
+            <h4 className="text-center text-lg font-semibold w-full mt-4">¡Elegí el miembro que te llame la atención para saber más  de él!</h4>
+
+            <div className="flex gap-4 overflow-x-auto py-4 px-2 scrollbar-none">
+                {miembrosData.map((member) => (
+                    <div
                         key={member.id}
-                        src={getAsset(member.img)}
-                        alt={member.nombre}
-                        onClick={() => onSelect(member.id)}
-                        className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-103"
-                    />
-                    <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
-                </div>
-            ))}
+                        className="relative shrink-0 w-65 h-auto rounded-lg overflow-hidden cursor-pointer group hover:scale-105 "
+                        onClick={() => onSelect(member.id)}>
+                        <img
+                            key={member.id}
+                            src={getAsset(member.img)}
+                            alt={member.nombre}
+                            onClick={() => onSelect(member.id)}
+                            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-103"
+                        />
+                        <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
