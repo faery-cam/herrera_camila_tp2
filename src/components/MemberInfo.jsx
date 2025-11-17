bf23b2c68aa67d966c0b7db1cbdb7f7f2a818322
Cam <herreracamila1304@gmail.com>
2025-11-17 00:30:21 -0300

    [STYLE] estilos en la sección de miembros

--- src/components/MemberInfo.jsx
+++ src/components/MemberInfo.jsx
@@ -1,8 +1,8 @@
 export default function MemberInfo({ activeMember }) {
     return (
-        <div>
-            <h3>{activeMember.nombre}</h3>
-            <p>{activeMember.bio}</p>
+        <div className="md:w-2/4 px-4 py-6 flex flex-col gap-4">
+            <h3 className="text-2xl font-bold tracking-tight">{activeMember.nombre}</h3>
+            <p className="text-base text-black-200">{activeMember.bio}</p>
         </div>
     );
 }
--- src/components/MemberMedia.jsx
+++ src/components/MemberMedia.jsx
@@ -1,11 +1,24 @@
+import { Link } from "react-router-dom"
+
 export default function MemberMedia({ activeMember }) {
     return (
-        <div>
-            <div>
-                <h4>Videos de {activeMember.nombre}</h4>
+        <div className="
+            grid grid-cols-2 gap-4
+            md:grid-cols-1">
+
+
+
+
+            <div className="px-4 py-3">
+                <h4 className="text-lg font-semibold">Videos de {activeMember.nombre}</h4>
             </div>
-            <div>
-                <h4>Fotos de {activeMember.nombre}</h4>
+
+
+
+
+
+            <div className="px-4 py-3">
+                <h4 className="text-lg font-semibold">Fotos de {activeMember.nombre}</h4>
             </div>
         </div>
     )
